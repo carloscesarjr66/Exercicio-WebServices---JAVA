c3e3aee94d17e6a3fd6707fdc0c0c39386c396c3
Carlos Cesar <cc.rod@hotmail.com>
2020-02-21 11:57:25 -0300

    Versao Inicial

---
+++ ExemploWs/src/br/com/fiap/estoque/EstoqueBasico.java
@@ -0,0 +1,21 @@
+package br.com.fiap.estoque;
+
+public class EstoqueBasico {
+
+	public int soma (int nr1, int nr2) {
+		return (nr1+nr2);
+	}
+	
+	public int subtrair (int nr1, int nr2) {
+		return (nr1-nr2);
+	}
+	
+	public int multiplicar (int nr1, int nr2) {
+		return (nr1*nr2);
+	}
+	
+	public int dividir (int nr1, int nr2) {
+		return (nr1/nr2);
+	}
+	
+}
